83fd6249677f39898789e9004df7f04665d76748
Mary Schmidt <marykateschmidt@gmail.com>
2023-09-04 16:01:20 -0700

    Set up firebase auth

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# super-special-layout-generator
- Layout Generator++
+# (Super Special Layout Generator)[https://super-special-layout-generator.web.app/]
+This is a tool to help folks generate layouts for given battery configurations. They need to be able to 
+1. Determine the resulting budget, land size, and energy requirements
+2. View a mock site layout complete with the number of transformers that would be required
+3. Save and resume their sessions across cache clears (thus, we aren't using localstorage)
+
+## Design Decisions
+
+## Architecture
+Tech stack
+- Vite
+- React
+- Typescript
+- Firebase (quick to get started and host, persistent data made easy)
+    - Realtime Database because it's tried and true & this is not a mobile application
+    - Drop in UI library for speed of development
+    - Longer term we could implement (offline persistence)[https://firebase.google.com/docs/database/web/offline-capabilities]
+
+## Notes
+- Runs on port `8000` locally.
+- Authentication and persistence are handled through Firebase
+
+## TODO
+- Security for the data -- this should be defined in `database.rules.json`.
+- Take control of auth UX
+- Analytics and telemetry
+- A/B testing
+- Ability to manage multiple builds at once (support users having multiple open build sessions at once)
+- Enable more authentication providers (built in to Firebase, easy to add. Docs available [here](https://firebase.google.com/docs/auth/web/firebaseui))
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'firebaseui/dist/firebaseui.css'
+
+// Import the functions you need from the SDKs you need
+// import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAyUuZ7oH8W59kzQDTgquP1Yj4kCVrMkNc",
+  authDomain: "super-special-layout-generator.firebaseapp.com",
+  projectId: "super-special-layout-generator",
+  storageBucket: "super-special-layout-generator.appspot.com",
+  messagingSenderId: "976149237124",
+  appId: "1:976149237124:web:4c8f112d76f4d01969a1d1",
+  measurementId: "G-W7WQ3SXKGL",
+  databaseURL: "https://super-special-layout-generator-default.rtdb.firebaseio.com",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+// const app = initializeApp(firebaseConfig);
+// const database = getDatabase(app);
+// Initialize the FirebaseUI Widget using Firebase.
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+});
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Super Special Layout Generator</h1>
+      <div id="firebaseui-auth-container" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,9 +52,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
