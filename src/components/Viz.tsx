9a6a248ca25d0f22d7503b983f1725415b22c0b6
Mary Schmidt <marykateschmidt@gmail.com>
2023-10-03 19:14:20 -0700

    chore: Refactor back to styled

--- src/components/Viz.tsx
+++ src/components/Viz.tsx
@@ -1,52 +1,65 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef } from "react";
 import { hierarchy } from "d3-hierarchy";
 import { select } from "d3-selection";
 import { scaleOrdinal } from "d3-scale";
 import { HierarchicalData } from "../models/Viz";
 import { schemePaired } from "d3-scale-chromatic";
 import { TEN } from "../utils/getFtToPxConversionFactor";
+import Grid from "./Grid";
+import Device from "./Device";
 
 interface VizProps {
   data: HierarchicalData;
 }
 
 const Viz = ({ data }: VizProps) => {
-  const vizRef = useRef<HTMLDivElement | null>(null);
+  const vizRef = useRef<HTMLDivElement>(null);
   const color = scaleOrdinal(
     data.children.map((d) => d.name),
     schemePaired
   );
 
-  const renderGrid = useCallback(() => {
-    const grid = select(vizRef?.current);
+  const leaves = useMemo(() => {
+    return hierarchy(data)
+      .leaves()
+      .filter((d) => !!d.data.value);
+    // const grid = select(vizRef?.current);
 
-    if (grid) {
-      const dataHierachy = hierarchy(data);
-      grid
-        .attr("id", "grid")
-        .style("display", "grid")
-        .style("grid-gap", "0px")
-        .style("grid-template-columns", `repeat(10, fit-content(10%))`)
-        .style("grid-auto-rows", `${TEN * TEN}px`)
-        .style("grid-auto-flow", "dense");
+    // if (grid) {
+    //   const dataHierachy = hierarchy(data);
+    // //   grid
+    // //     .attr("id", "grid")
+    // //     .style("display", "grid")
+    // //     .style("grid-gap", "0px")
+    // //     .style("grid-template-columns", `repeat(10, fit-content(10%))`)
+    // //     .style("grid-auto-rows", `${TEN * TEN}px`)
+    // //     .style("grid-auto-flow", "dense");
 
-      grid
-        .selectAll("div")
-        .data(dataHierachy.leaves().filter((d) => !!d.data.value))
-        .join("div")
-        .style("background-color", (d) => color(d.data.name))
-        .style("height", `${TEN * TEN}px`)
-        .style("width", (d) => `${d.data.value * TEN}px`)
-        .style("grid-column-end", (d) => `span ${d.data.value / TEN}`);
-    }
-  }, [data, color]);
+    //   grid
+    //     .selectAll("div")
+    //     .data(dataHierachy.leaves().filter((d) => !!d.data.value))
+    //     .join("div")
+    //     .style("background-color", (d) => color(d.data.name))
+    //     .style("height", `${TEN * TEN}px`)
+    //     .style("width", (d) => `${d.data.value * TEN}px`)
+    //     .style("grid-column-end", (d) => `span ${d.data.value / TEN}`);
+    // }
+  }, [data]);
 
-  // Update loop
-  useEffect(() => {
-    renderGrid();
-  }, [renderGrid]);
-
-  return <div ref={vizRef} />;
+  return (
+    <Grid ref={vizRef}>
+      {leaves.map((leaf) => {
+        console.log("AAA", leaf.data.value);
+        return (
+          <Device
+            key={leaf.data.id}
+            backgroundColor={color(leaf.data.name)}
+            widthFt={leaf.data.value}
+          />
+        );
+      })}
+    </Grid>
+  );
 };
 
 export default Viz;
