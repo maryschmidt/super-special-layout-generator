eca6ff7f5b09e1ec786ce75964f173e600e2f176
Mary Schmidt <marykateschmidt@gmail.com>
2023-10-03 19:16:49 -0700

    chore: Cleanup comments, unused packages

--- src/components/Viz.tsx
+++ src/components/Viz.tsx
@@ -1,10 +1,8 @@
-import { useCallback, useEffect, useMemo, useRef } from "react";
+import { useMemo, useRef } from "react";
 import { hierarchy } from "d3-hierarchy";
-import { select } from "d3-selection";
 import { scaleOrdinal } from "d3-scale";
 import { HierarchicalData } from "../models/Viz";
 import { schemePaired } from "d3-scale-chromatic";
-import { TEN } from "../utils/getFtToPxConversionFactor";
 import Grid from "./Grid";
 import Device from "./Device";
 
@@ -19,32 +17,13 @@ const Viz = ({ data }: VizProps) => {
     schemePaired
   );
 
-  const leaves = useMemo(() => {
-    return hierarchy(data)
-      .leaves()
-      .filter((d) => !!d.data.value);
-    // const grid = select(vizRef?.current);
-
-    // if (grid) {
-    //   const dataHierachy = hierarchy(data);
-    // //   grid
-    // //     .attr("id", "grid")
-    // //     .style("display", "grid")
-    // //     .style("grid-gap", "0px")
-    // //     .style("grid-template-columns", `repeat(10, fit-content(10%))`)
-    // //     .style("grid-auto-rows", `${TEN * TEN}px`)
-    // //     .style("grid-auto-flow", "dense");
-
-    //   grid
-    //     .selectAll("div")
-    //     .data(dataHierachy.leaves().filter((d) => !!d.data.value))
-    //     .join("div")
-    //     .style("background-color", (d) => color(d.data.name))
-    //     .style("height", `${TEN * TEN}px`)
-    //     .style("width", (d) => `${d.data.value * TEN}px`)
-    //     .style("grid-column-end", (d) => `span ${d.data.value / TEN}`);
-    // }
-  }, [data]);
+  const leaves = useMemo(
+    () =>
+      hierarchy(data)
+        .leaves()
+        .filter((d) => !!d.data.value),
+    [data]
+  );
 
   return (
     <Grid ref={vizRef}>
